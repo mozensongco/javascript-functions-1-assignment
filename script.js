cec032a0fd676d822ac04f1f37f115f277b0b929
Mozen Songco <fsongco@gmail.com>
2023-06-22 22:34:19 -0400

    Add changes as directed in the exercises

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 //After you're done, try looking around at the CSS and HTML to 
 //see if you understand what's going on :)
 
+function numberAdder(num1, num2) {
+    return num1 + num2;
+}
+
+/* Seems like the HTML outlines two forms for the numbers
+and the CSS customizes the labels, form boxes, and buttons.
+Also, in retrospect it makes a lot of sense for CSS to control the cursor shape...
+I wonder if there's a way to style that with a custom image to replace the cursor in CSS? */
